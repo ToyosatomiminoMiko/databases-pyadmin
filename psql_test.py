ad461b125ff0330c92c4f96b6130ac5b68b92b17
ToyosatomiminoMiko <63940347+ToyosatomiminoMiko@users.noreply.github.com>
2022-11-15 19:21:33 +0800

    Update psql_test.py

--- psql_test.py
+++ psql_test.py
@@ -1,16 +1,18 @@
 import psycopg2
+
 import tools
 
 
 # 创建连接对象
 # psql version = 15
 
+
 def connect_db():
     try:
         connect = psycopg2.connect(
             database='postgres',
-            user='postgres',
-            password='19260817',
+            user='senpai',
+            password='114514',
             host='127.0.0.1',
             port='5432'
         )
@@ -34,12 +36,11 @@ if __name__ == '__main__':
     # 创建游标对象
     cur = conn.cursor()
 
-    cur.execute("select * from movie;")
+    cur.execute("select * from person;")
     results = cur.fetchall()
-    print(results)
-    # cur.execute("INSERT INTO movie (movie_id, movie_title, director, author, actor, submission_date) \
-    # VALUES (1, 'python', 'myself', 'yourself', 'himself', current_date);")
-
+    for r in results:
+        print(r)
+    # cur.execute(f"INSERT INTO person (person_id, name) VALUES (1,'python');")
     # 关闭游标
     cur.close()
     close_db_connection(conn)
